14d56b3c0f7e0dd26f42b4706b689fcf7ac0ee0a
Julia Almada <almadajuka2501@gmail.com>
2025-06-02 15:01:46 -0300

    Estudo de POO com js

---
+++ Admin.js
@@ -0,0 +1,12 @@
+import User from "./User.js";
+
+export default class Admin extends User {
+  constructor(nome, email, nascimento, role = "admin", ativo = "true") {
+    super(nome, email, nascimento, role, ativo)
+  }
+
+  criarCurso(nomeCurso, qtdVagas) {
+    return `curso ${nomeCurso} criado com ${qtdVagas} vagas.`
+  }
+}
+
